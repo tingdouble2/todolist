6f76046c799e7c07063a74d0f0f6d563de1c35b3
Sarocha Boriraklerd <sarochaboriraklerd@gmail.com>
2021-06-07 13:53:15 +0700

    add class

--- components/List.vue
+++ components/List.vue
@@ -1,11 +1,14 @@
 <template>
-  <ul  class="item">
+<div class="list-item">
+   <ul  class="item">
       <li v-for="list,index in lists" :key="index">
         <input type="checkbox" class="checkbox" v-model="list.done">
           <span  :class="{done : list.done }"> {{list.title}}</span>
         <button class="delete-item" @click="$emit('OnRemove' ,index)">X</button>
       </li>
     </ul>
+</div>
+
 </template>
 
 <script>
